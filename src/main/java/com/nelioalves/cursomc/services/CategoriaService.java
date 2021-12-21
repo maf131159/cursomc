8aefc247028d6b46e9d57c8f1fb75ec1a2ad7eb4
maf131159 <manoelantoniofernandes@gmail.com>
2021-12-21 16:16:22 -0300

    validacao sintatica com Bean validation(39/40)

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.dto.CategoriaDTO;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
 import com.nelioalves.cursomc.services.exceptions.DataIntegretyException;
 import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
@@ -60,4 +61,7 @@ public class CategoriaService {
 		return repo.findAll(pageRequest);
 	}
 	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+	}
 }
